cf4c744393803f29eaa08abea85b82aa07b36a48
Arge377 <arcangioli11@gmail.com>
2022-03-09 20:49:37 -0300

    eliminar imagenes

--- src/img/Books/HarryPotter/index.js
+++
@@ -1,7 +0,0 @@
-export {default  as HarryPotter1} from './piedra_filosofal.jpg';
-export {default  as HarryPotter2} from './camara_secreta.jpg';
-export {default  as HarryPotter3} from './prisionero_de_azkaban.jpg';
-export {default  as HarryPotter4} from './caliz_de_fuego.jpg';
-export {default  as HarryPotter5} from './orden_del_fenix.jpg';
-export {default  as HarryPotter6} from './misterio_del_principe.jpg';
-export {default  as HarryPotter7} from './reliquias_de_la_muerte.jpg';
--- src/img/Books/Narnia/index.js
+++
@@ -1,7 +0,0 @@
-export {default  as Narnia1} from './el-leon-la-bruja-y-el-armario.jpg';
-export {default  as Narnia2} from './el-principe-caspian.jpg';
-export {default  as Narnia3} from './viajero-del-alba.jpg';
-export {default  as Narnia4} from './silla-de-plata.jpg';
-export {default  as Narnia5} from './el-caballo-y-el-muchacho.jpg';
-export {default  as Narnia6} from './el-sobrino-del-mago.jpg';
-export {default  as Narnia7} from './la-ultima-batalla.jpg';
